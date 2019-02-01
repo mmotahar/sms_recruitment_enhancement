176c7c1021d96d0bf6e9f974170f345da7efbec9
nils <nils@trobz.com>
2019-02-01 10:52:16 +0000

    Squashed 'addons/server-ux/' content from commit 7b7cad771c5
    git-subtree-dir: addons/server-ux
    git-subtree-split: 7b7cad771c53b9110b3d5b833f60d225bf8dbf56

---
+++ setup/_metapackage/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo12-addons-oca-server-ux",
+    description="Meta package for oca-server-ux Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo12-addon-base_technical_features',
+        'odoo12-addon-date_range',
+        'odoo12-addon-mass_editing',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+    ]
+)
