2b50f1a06b899f1705a9a2315728a890e4ce554a
Danh Vo <danh@trobz.com>
2019-04-12 11:24:31 +0700

    [REF]Removing project for subtree configuration

--- project/sms_recruitment_enhancement/__init__.py
+++
@@ -1,5 +0,0 @@
-##############################################################################
-#    Copyright (C) Ioppolo and Associates (I&A) 2018 (<http://ioppolo.com.au>).
-##############################################################################
-from . import models
-from . import controllers
--- project/sms_recruitment_enhancement/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import survey_question
-from . import survey_user_input_line
-from . import survey_survey
